85a40fd2fa8301856837ffd92a9a70a4637945b0
Prathamdas3 <116100433+Prathamdas3@users.noreply.github.com>
2023-06-16 12:53:28 +0530

    working on the second part of the business part of my web site

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,6 +2,14 @@ import { features } from "../constants";
 import styles, { layout } from "../style";
 import Button from "./Button";
 
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div>
+    <div>
+      <img src={icon} alt="icon" />
+    </div>
+  </div>
+);
+
 const Business = () => {
   return (
     <section id="features" className={layout.section}>
@@ -19,6 +27,12 @@ const Business = () => {
         </p>
 
         <Button styles="mt-10" />
+
+        <div className={`${layout.sectionImg} flex-col `}>
+          {features.map((feature, index) => {
+            <FeatureCard key={feature.id} {...feature} index={index} />;
+          })}
+        </div>
       </div>
     </section>
   );
